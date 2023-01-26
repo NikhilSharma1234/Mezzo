8ffea408a93971263fd3efa9ea415f1cb63bb39b
Nikhil <nikhil342015@gmail.com>
2023-01-25 20:10:13 -0800

    build + backend start

---
+++ mezzo/server/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  securityQ1: {
+    type: String,
+    required: true,
+  },
+  securityQ2: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: 'https://via.placeholder.com/150'
+  },
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+});
+
+
+// Insert Password Hash Here
+
+// Insert Password Validation Here
+
+// Password Reset Here --> Insert Security Q(s) Validation Here
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
