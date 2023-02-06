04c29e4985381122187f104b42fbff04d2b90107
Nikhil <nikhil342015@gmail.com>
2023-02-05 18:45:48 -0800

    Playlist endpoints

--- mezzo/server/api.js
+++ mezzo/server/api.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+require('mongoose').Types.ObjectId;
 const { Router } = require("express");
 const apiRouter = Router();
 const User = require("./models/User");
+const Playlist = require("./models/Playlist")
 const Token = require("./models/Token");
 const {forgotPassword, resetPassword} = require("./password_reset");
 const JWT = require("jsonwebtoken");
+const { ObjectId } = require("mongodb");
 const axios = require('axios').default;
 
 // User Constraints
@@ -23,7 +26,7 @@ apiRouter.get("/user/:id", async (req, res) => {
   }
 });
 
-apiRouter.post('/register/user', async (req, res) => {
+apiRouter.post('/user/register', async (req, res) => {
   try {
     const newUser = await User({ ...req.body });
     if (!emailReg.test(newUser.email)){
@@ -50,7 +53,7 @@ apiRouter.post('/register/user', async (req, res) => {
   }
 });
 
-apiRouter.post('/login/user', function(req, res) {
+apiRouter.post('/user/login', function(req, res) {
   User.findOne({username: req.body.username}, async function(err, user) {
     if (!user){
       console.log("User doesn't exist");
@@ -64,6 +67,105 @@ apiRouter.post('/login/user', function(req, res) {
         console.log("Incorrect Password");
         //redirect here...
         res.send("nope");
+      }
+    }
+  });
+});
+
+apiRouter.post('/playlist/create', function(req, res) {
+  User.findOne({username: req.body.username}, async function(err, user) {
+    if (!user){
+      res.send("User doesn't exist");
+    } else {
+        try {
+          const newPlaylist = await Playlist({ ...req.body });
+          newPlaylist.user = user._id;
+          await newPlaylist.save();
+          res.json({ id: newPlaylist._id });
+        } catch (err) {
+          console.error(err);
+          res.sendStatus(400);
+        }
+      }
+  });
+});
+
+apiRouter.put('/playlist/add', function(req, res) {
+  User.findOne({username: req.body.username}, async function(err, user) {
+    if (!user){
+      console.log("User doesn't exist");
+    } else if(!req.body.song_id) {
+        res.send("No song id");
+    }
+      else {
+        try {
+          Playlist.findById(req.body.id, async function(err, playlist) {
+            if (!playlist) {
+              res.send("Playlist not found");
+            }
+            else {
+              if (playlist.songs.includes(req.body.song_id)) res.status(404).send("Song already exists in playlist");
+              playlist.songs.push(req.body.song_id);
+              playlist.updatedAt = new Date();
+              playlist.save();
+              res.json({song_ids: playlist.songs});
+            }
+          });
+        } catch (err) {
+          console.error(err);
+          res.status(400).send(err);
+        }
+      }
+  });
+});
+
+apiRouter.put('/playlist/remove', function(req, res) {
+  User.findOne({username: req.body.username}, async function(err, user) {
+    if (!user){
+      res.send("username does not exist");
+    } else if(!req.body.song_id) {
+        res.send("No song id");
+    }
+      else {
+        try {
+          Playlist.findById(req.body.id, async function(err, playlist) {
+            if (!playlist) {
+              res.send("Playlist not found");
+            }
+            else {
+              const songs = playlist.songs;
+              playlist.songs = songs.filter(song_id => song_id !== req.body.song_id);
+              playlist.updatedAt = new Date();
+              playlist.save();
+              res.json({song_ids: playlist.songs});
+            }
+          });
+        } catch (err) {
+          res.send(err);
+          res.sendStatus(400);
+        }
+      }
+  });
+});
+
+apiRouter.get('/playlist', function(req, res) {
+  User.findOne({username: req.body.username}, async function(err, user) {
+    if (!user){
+      res.send("username does not exist");
+    }
+    else {
+      try {
+        Playlist.findById(req.body.id, async function(err, playlist) {
+          if (!playlist) {
+            res.send("Playlist not found");
+          }
+          else {
+            res.json({playlist: playlist});
+          }
+        });
+      } catch (err) {
+        res.send(err);
+        res.sendStatus(400);
       }
     }
   });
