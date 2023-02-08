8f00d570bc0130db25bd7f209965afbf00bd4240
fchau1 <fchau@nevada.unr.edu>
2023-02-08 01:48:07 -0800

    Added CSS grid to cards and other front-end

---
+++ mezzo/src/components/cards/artist-card.js
@@ -0,0 +1,50 @@
+import './song-card.scoped.css'
+import React, { useEffect, useRef } from 'react';
+
+const ImageContainer = ({ src, alt }) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      const aspectRatio = image.naturalWidth / image.naturalHeight;
+      containerRef.current.style.paddingBottom = `${100 / aspectRatio}%`;
+    };
+  }, [src]);
+
+  return (
+    <div ref={containerRef} className="image-container">
+      <img className="artist-photo" src={src} alt={alt} />
+    </div>
+  );
+};
+
+
+function ArtistCard ({artistData}) {
+      return (
+      <div className="artist-card">
+        <div>
+        
+          <img className="artist-photo" src={artistData.images[1].url} alt='artistImage' />
+        </div>
+       
+        <h3>{artistData.name}</h3>
+      </div>
+    );
+}
+
+function ArtistCards({artists}) {
+  return (
+      <div className="card-grid" id="card-grid">
+      {artists.map((value, key)=>{
+          if(value.images && value.images.length > 0){
+            return <ArtistCard artistData={value}/>
+          }
+        })
+        }
+      </div>
+  );
+}
+
+export default ArtistCards
--- mezzo/src/components/cards/song-card.js
+++ mezzo/src/components/cards/song-card.js
@@ -1,4 +1,5 @@
 import { FaPlay } from 'react-icons/fa';
+import {FaHeart} from 'react-icons/fa';
 import './song-card.scoped.css'
 
 function PlayButton (props) {
@@ -9,35 +10,41 @@ function PlayButton (props) {
   );
 }
 
-
+function Heart (props) {
+  return (
+    <button className="play_button">
+      <FaHeart size={15} color="var(--song-card-color)"/>
+    </button>
+  );
+}
 
 function SongCard ({songData}) {
   return (
-    <article className="card">
+    <div className="card">
       <div>
         <img src={songData.album.images[1].url} alt='albumImage' />
       </div>
 
       <div className="card-body">      
-      <h2>{songData.name}</h2>
-      <p>artist</p>
-      <PlayButton/>
+        <h2>{songData.name}</h2>
+        <div className="card-caption">
+          <p>artist</p>
+          <Heart/>
+          <PlayButton/>
+        </div>
+      </div>
     </div>
-    </article>
   );
 }
 
 function SongCards ({songs}) {  
   return (
-    <div>
-      <div className="app-card-list" id="app-card-list">
+      <div className="card-grid">
         {songs?.map((value, key)=>{
           return <SongCard songData={value}/>
         })
-        }
+      }
       </div>
-      
-    </div>
   );
 }
 
