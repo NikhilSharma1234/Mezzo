903eb986476a1348776342e40251588b122bf118
fchau1 <fchau.nevada.unr.edu>
2023-02-08 17:48:15 -0800

    endpoints

--- mezzo/server/api.js
+++ mezzo/server/api.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const { Router } = require("express");
 const router = Router();
-
+const User = require("./models/User.js");
+const Token = require("./models/Token");
+const {forgotPasswordHandler, resetPasswordHandler} = require("./password_reset");
+const JWT = require("jsonwebtoken");
 const PlaylistsController = require("./controllers/PlaylistsController.js");
 const UsersController = require("./controllers/UsersController.js");
 
 router.post('/user', UsersController.newUser);
-router.post('/user/login', UsersController.loginUser);
+//router.post('/user/login', UsersController.loginUser);
 router.post('/user/logout', UsersController.logoutUser);
 router.put('/user/password', UsersController.forgotPassword);
 router.get('/user/password', UsersController.resetPassword);
@@ -17,5 +20,24 @@ router.get('/playlist/all', PlaylistsController.getAllPlaylists);
 router.delete('/playlist', PlaylistsController.deletePlaylist);
 router.put('/playlist/add', PlaylistsController.addSong);
 router.put('/playlist/remove', PlaylistsController.removeSong);
+
+
+router.post('/user/login', async (req, res) => {
+  console.log("at endpoint")
+    User.findOne({username: req.body.username}, async function(err, user) {
+      if (!user){
+        console.log("User doesn't exist");
+      } else {
+        const matchedPasswords = await user.validPassword(req.body.password, user.password);
+        if (matchedPasswords) {
+          console.log("Correct Password");
+          return res.send({success: true});
+        } else {
+          console.log("Incorrect Password");
+          return res.status(401).send({error: 'Incorrect password'});
+        }
+      }
+    });
+});
 
 module.exports = router;
