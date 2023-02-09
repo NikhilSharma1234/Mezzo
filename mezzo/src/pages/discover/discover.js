5d7b313c1cd5eea38dda28604bb44f073f1b75cd
fchau1 <fchau@nevada.unr.edu>
2023-02-08 17:45:45 -0800

    minor changes to discover page

--- mezzo/src/pages/discover/discover.js
+++ mezzo/src/pages/discover/discover.js
@@ -1,14 +1,13 @@
 import SearchBar from "../../components/searchbar/searchbar.js";
-import SongCards from '../../components/cards/song-card.js'
-import ArtistCards from '../../components/cards/artist-card.js'
-import './discover.scoped.css';
+import SongCards from "../../components/cards/song-card.js";
+import ArtistCards from "../../components/cards/artist-card.js";
+import "./discover.scoped.css";
 import { fetchArtistTopTracks } from "../../utils/fetchArtistTopTracks.js";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { fetchSearchResults } from "../../utils/fetchSearchResults.js";
 
 const Discover = () => {
-
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [songs, setSongs] = useState([]);
   const [artists, setArtists] = useState([]);
   const updateSearchInput = (newSearchInput) => {
@@ -22,48 +21,47 @@ const Discover = () => {
     };
 
     const fetchTracksData = async () => {
-      const tracksData = await fetchSearchResults(searchInput.name, 'track');
+      const tracksData = await fetchSearchResults(searchInput.name, "track");
       setSongs(tracksData);
     };
 
     const fetchArtistsData = async () => {
-      const similarArtists = await fetchSearchResults(searchInput.name, 'artist');
+      const similarArtists = await fetchSearchResults(
+        searchInput.name,
+        "artist"
+      );
       setArtists(similarArtists);
-    }
+    };
 
-    if (searchInput){
-      if (searchInput.type && searchInput.type === "artist"){
-        fetchTopTracksData(); 
-      } else{
+    if (searchInput) {
+      if (searchInput.type && searchInput.type === "artist") {
+        fetchTopTracksData();
+      } else {
         fetchTracksData();
       }
       fetchArtistsData();
     }
   }, [searchInput]);
-  
 
   return (
     <section className="main main_closed">
+      <SearchBar
+        placeholder="Enter a song/artist"
+        searchInput={searchInput}
+        setSearchInput={updateSearchInput}
+      />
+      <section className="Songs-Section">
         <div>
-            <SearchBar placeholder="Enter a song/artist" searchInput={searchInput} setSearchInput={updateSearchInput}/>
-            <section className="Songs-Section">
+          <h3 id="SongHeading">Songs</h3>
 
-            <div>
-            <h3 id="SongHeading">Songs</h3>
-
-            {songs.length !== 0 && (
-                <SongCards songs={songs}/> )}
-            </div>
-
-            <div>
-            <h3 id="ArtistHeading">Artists</h3>
-            {artists.length !== 0 && (
-                <ArtistCards artists={artists}/> )}
-            </div>
-
-            </section>
-            
+          {songs.length !== 0 && <SongCards songs={songs} />}
         </div>
+
+        <div>
+          <h3 id="ArtistHeading">Artists</h3>
+          {artists.length !== 0 && <ArtistCards artists={artists} />}
+        </div>
+      </section>
     </section>
   );
 };
