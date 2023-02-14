abbf8f2e2b5b47cdbf6698f1fb6c2c04797a2e5e
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2023-02-13 16:56:40 -0800

    Quick Patch

--- mezzo/server/api.js
+++ mezzo/server/api.js
@@ -9,6 +9,7 @@ const PlaylistsController = require("./controllers/PlaylistsController.js");
 const UsersController = require("./controllers/UsersController.js");
 
 router.post('/user', UsersController.newUser);
+router.post('/user/login', UsersController.loginUser);
 router.post('/user/logout', UsersController.logoutUser);
 router.post('/user/password', UsersController.forgotPassword);
 router.get('/user/password', UsersController.resetPassword);
@@ -19,26 +20,5 @@ router.get('/playlist/all', PlaylistsController.getAllPlaylists);
 router.delete('/playlist', PlaylistsController.deletePlaylist);
 router.put('/playlist/add', PlaylistsController.addSong);
 router.put('/playlist/remove', PlaylistsController.removeSong);
-
-
-router.post('/user/login', async (req, res) => {
-  User.findOne({username: req.body.username}, async function(err, user) {
-      if (err) {
-        return res.status(500).send({error: 'Error while trying to find user'});
-      }
-      if (!user){
-        console.log("User doesn't exist");
-      } else {
-        const matchedPasswords = await user.validPassword(req.body.password, user.password);
-        if (matchedPasswords) {
-          console.log("Correct Password");
-          return res.send({success: true});
-        } else {
-          console.log("Incorrect Password");
-          return res.status(401).send({error: 'Incorrect password'});
-        }
-      }
-    });
-});
 
 module.exports = router;
