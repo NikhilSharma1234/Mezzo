cd7198622531860c9c388e63f5ee559e7ae2c452
fchau1 <fchau@nevada.unr.edu>
2023-02-15 18:11:31 -0800

    Routing for login and logout

--- mezzo/src/components/navbar/navbar_popup.js
+++ mezzo/src/components/navbar/navbar_popup.js
@@ -1,38 +1,63 @@
-import '../../assets/global.scoped.css';
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import "../../assets/global.scoped.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 import { Link } from "react-router-dom";
-import './navbar_popup.css';
+import "./navbar_popup.css";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 function NavbarPopup() {
-  function handleLogout (){
-    fetch('http://localhost:4000/api/user/logout', {
-      method: 'POST' 
-    })
-  }
+  const [logoutStatus, setLogoutStatus] = useState(null);
+
+
+  const handleLogout = async () => {
+    try {
+      const logoutStat = await fetch("http://localhost:4000/api/user/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({success: true})
+      })
+      console.log("logoutStat");
+      console.log(logoutStat);
+      setLogoutStatus(logoutStat);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <PopupState variant="popover" className="options_popup">
-      {(popupState) => (
-        <React.Fragment>
-          <Button variant="contained" {...bindTrigger(popupState)}>
-            ...
-          </Button>
-          <Menu {...bindMenu(popupState)}>
-            <MenuItem onClick={popupState.close}>View</MenuItem>
-            <MenuItem onClick={popupState.close}>Friends</MenuItem>
-            <MenuItem onClick={popupState.close}>Playback</MenuItem>
-            <MenuItem onClick={popupState.close}>Help</MenuItem>
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-          </Menu>
-        </React.Fragment>
-      )}  
-    </PopupState>
+    <>
+      {logoutStatus && (
+        <Navigate
+          to={{
+            pathname: "..",
+          }}
+          replace={true}
+        />
+      )}
+      <PopupState variant="popover" className="options_popup">
+        {(popupState) => (
+          <React.Fragment>
+            <Button variant="contained" {...bindTrigger(popupState)}>
+              ...
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+              <MenuItem onClick={popupState.close}>View</MenuItem>
+              <MenuItem onClick={popupState.close}>Friends</MenuItem>
+              <MenuItem onClick={popupState.close}>Playback</MenuItem>
+              <MenuItem onClick={popupState.close}>Help</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </React.Fragment>
+        )}
+      </PopupState>
+    </>
   );
 }
-
 
 export default NavbarPopup;
