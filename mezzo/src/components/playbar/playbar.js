83838630f4e0ccb3522e30746994e88e12584c23
fchau1 <fchau@nevada.unr.edu>
2023-02-15 18:12:33 -0800

    Added functionality to playbar through context and it's also connected to the play buttons for each song

--- mezzo/src/components/playbar/playbar.js
+++ mezzo/src/components/playbar/playbar.js
@@ -11,9 +11,28 @@ import Stack from '@mui/material/Stack';
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { ImVolumeMedium, ImVolumeLow} from 'react-icons/im';
+import {AudioContext } from '../../context/audioContext.js';
+import {useState, useEffect, useContext} from 'react';
 
 const PlayBar = () => {
-    const [value, setValue] = React.useState(0);
+
+    const [playerInfo,, isPlaying, togglePlayer] = useContext(AudioContext);
+    const [audio, setAudio] = useState(null);
+    const [playBtn, setPlayerBtn] = useState(null);
+    const [value, setValue] = useState(0);
+
+    useEffect(() => {
+        console.log("play song")
+        console.log(playerInfo.audioUrl)
+        const audioElement = new Audio(playerInfo.audioUrl);
+        if (isPlaying) {
+            audioElement.volume = 0.03;
+            audioElement.play();
+            setAudio(audioElement);
+          } else {
+            audio.pause();
+          }
+    }, [isPlaying]);
 
     return (
         <section className="main_closed main">
@@ -23,9 +42,8 @@ const PlayBar = () => {
                 <BottomNavigation className="playbar" showLabels value={value}
                     onChange={(event, newValue) => {setValue(newValue);}}>
                     <BottomNavigationAction icon={<AiOutlineLeft />} />
-                    <BottomNavigationAction icon={<BsFillPlayFill />} />
-                    <BottomNavigationAction label="Song Title Text Here" />
-                    <BottomNavigationAction icon={<BsPauseFill />} />
+                    <BottomNavigationAction label={playerInfo.songName || "Song Title"} />
+                    <BottomNavigationAction onClick={togglePlayer} icon={isPlaying ? <BsPauseFill /> : <BsFillPlayFill />} />
                     <BottomNavigationAction icon={<AiOutlineRight />} />
                     <Box sx={{ width: 200, pt: 2 }}>
                         <Stack spacing={2} direction="row" alignItems="center">
