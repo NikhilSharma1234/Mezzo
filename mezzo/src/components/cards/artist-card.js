1b2f95543a2366c0351daf508a616412a8338bd5
fchau1 <fchau.nevada.unr.edu>
2023-02-21 20:46:51 -0800

    minor changes

--- mezzo/src/components/cards/artist-card.js
+++ mezzo/src/components/cards/artist-card.js
@@ -1,50 +1,38 @@
-import './card.scoped.css'
-import React, { useEffect, useRef } from 'react';
+import "./card.scoped.css";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ImageContainer = ({ src, alt }) => {
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const image = new Image();
-    image.src = src;
-    image.onload = () => {
-      const aspectRatio = image.naturalWidth / image.naturalHeight;
-      containerRef.current.style.paddingBottom = `${100 / aspectRatio}%`;
-    };
-  }, [src]);
+function ArtistCard({ artistData }) {
+  const navigate = useNavigate();
+  const navigateArtist = () => {
+    navigate(`/_/artist/${artistData.id}`, { replace: true });
+  };
 
   return (
-    <div ref={containerRef} className="image-container">
-      <img className="artist-photo" src={src} alt={alt} />
+    <button onClick={() => navigateArtist()} className="artist-card">
+      <div>
+        <img
+          className="artist-photo"
+          src={artistData.images[1].url}
+          alt="artistImage"
+        />
+      </div>
+
+      <h3>{artistData.name}</h3>
+    </button>
+  );
+}
+
+function ArtistCards({ artists }) {
+  return (
+    <div className="card-grid" id="card-grid">
+      {artists.map((value, key) => {
+        if (value.images && value.images.length > 0) {
+          return <ArtistCard artistData={value} />;
+        }
+      })}
     </div>
   );
-};
-
-
-function ArtistCard ({artistData}) {
-      return (
-      <div className="artist-card">
-        <div>
-        
-          <img className="artist-photo" src={artistData.images[1].url} alt='artistImage' />
-        </div>
-       
-        <h3>{artistData.name}</h3>
-      </div>
-    );
 }
 
-function ArtistCards({artists}) {
-  return (
-      <div className="card-grid" id="card-grid">
-      {artists.map((value, key)=>{
-          if(value.images && value.images.length > 0){
-            return <ArtistCard artistData={value}/>
-          }
-        })
-        }
-      </div>
-  );
-}
-
-export default ArtistCards
+export default ArtistCards;
